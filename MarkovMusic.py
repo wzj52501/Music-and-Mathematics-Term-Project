d832ba078e571b37f3eb9167af6512672efc5edb
wzj52501 <songxiaomingshiren@163.com>
2020-12-19 14:57:11 +0800

    Take advantage of eval.py

--- MarkovMusic.py
+++ MarkovMusic.py
@@ -80,20 +80,25 @@ if __name__ == '__main__':
     # 初始值需要传order个note和interval，order是Markov Chain的阶数
     generator = MusicGenerator(markov_instance, note_list[:order], interval_list[:order])
     
-    note_gen=[]
-    interval_gen=[]
-    for i in range(0,100):
-        #不断调用generator的next_state方法获取下一个音符
-        next_state = generator.next_state()
-        note_gen.append(next_state[0])
-        interval_gen.append(next_state[1])
-    print(note_gen)
-    print(interval_gen)
-    print(evaluate(note_gen,interval_gen,note_list,interval_list))
-    
-    gen_stream=generate_stream(note_gen, interval_gen, tune)
-    gen_stream.show('midi')
-    save_stream(gen_stream,'test/','music8')
+    best=0.0
+
+    for loop in range(1024):
+	    note_gen=[]
+	    interval_gen=[]
+	    for i in range(0,100):
+	        #不断调用generator的next_state方法获取下一个音符
+	        next_state = generator.next_state()
+	        note_gen.append(next_state[0])
+	        interval_gen.append(next_state[1])
+
+	    ret=evaluate(note_gen,interval_gen,note_list,interval_list)
+	    print(loop, ret)
+	    if(ret > best):
+	    	best=ret
+	    	gen_stream=generate_stream(note_gen, interval_gen, tune)
+    		#gen_stream.show('midi')
+    		save_stream(gen_stream,'test/','score='+str(ret))
+
 
     ori_stream=generate_stream(note_list, interval_list, tune)
     save_stream(ori_stream,'test/','orimusic8')
